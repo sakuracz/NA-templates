1051c60171546083fb2f0e91b9dead1d887b68e1
Michal Kozub <memozidozu.rinkimirikata@gmail.com>
2016-11-09 14:18:34 +0100

    Pierwsze zmiany w 10-algos/implementation.cpp - ladniejszy kod

--- 10-algos/implementation.cpp
+++ 10-algos/implementation.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <functional>
 
+/*
 type * find_return_type(const std::vector<function> & overload_set, std::vector<type *> argument_types)
 {
     for (std::vector<function>::const_iterator it = overload_set.begin(); it != overload_set.end(); ++it)
@@ -36,5 +37,25 @@ type * find_return_type(const std::vector<function> & overload_set, std::vector<
     }
 
     return nullptr;
-}
+}	*/
 
+type * find_return_type(const std::vector<function> & overload_set, std::vector<type *> argument_types)
+{
+	auto overload = std::find_if(overload_set.begin(), overload_set.end(), [&](auto && overload) {
+        if (argument_types.size() != overload.arguments.size())
+        {
+            return false;
+        }
+
+        return std::inner_product(argument_types.begin(), argument_types.end(), overload.arguments.begin(), true,
+            std::logical_and<>(),
+            [](auto && type, auto && variable){ return type == variable.type; });
+    });
+
+    if (overload == overload_set.end())
+    {
+        return nullptr;
+    }
+
+    return overload->return_type;
+}
--- 10-algos/implementation.h
+++ 10-algos/implementation.h
@@ -18,4 +18,5 @@ struct function
 };
 
 type * find_return_type(const std::vector<function> & overload_set, std::vector<type *> argument_types);
+//type * find_and_fuck_u(const std::vector<function> & overload_set, std::vector<type *> argument_types);
 
