8bb4d6881a32f35eae710cf52224b4549bdc9f72
Michal Kozub <memozidozu.rinkimirikata@gmail.com>
2016-11-09 15:50:05 +0100

    Second commit that includes basic changes that allow for the compilation of run-21-container wrapper

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <initializer_list>
 
 template<typename T>
-class container_wrapper;
+class container_wrapper
+{
+public:
+//    container_wrapper(std::initializer_list<T> l) : elem_count(l.count())
+//    {}
+
+    container_wrapper(T arg) : elem_count(0), _value(arg){}
+    container_wrapper() = default;
+
+
+    size_t size() const { return _value.size(); }
+
+private:
+    std::size_t elem_count;
+    T _value;
+
+};
 
