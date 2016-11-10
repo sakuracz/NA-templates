c90e5fbbcf29b618a475232574b6d79f35dd58a2
Michal Kozub <memozidozu.rinkimirikata@gmail.com>
2016-11-10 15:47:08 +0100

    Proper implementation according to requirements

--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -8,20 +8,9 @@
 vector_erase(data, 1);
 REQUIRE((data == std::vector<int>{ 2, 3, 3, 2 })); */
 
-template<typename C, typename T>
-void vector_erase(C& cont, T toDrop){
- //   int dropCount = 0;
- //   std::cout << cont.size() << "\t" << dropCount << std::endl;
+template<typename C>
+void vector_erase(C& cont, typename C::value_type toDrop){
 
-//    for(auto i = cont.begin(); i != cont.end(); i++)
-//    {
-//       auto j = std::find(i, cont.end(), toDrop );
-//       i = cont.erase(j);
-//       i = cont.erase(std::find(i, cont.end(), toDrop ));
         cont.erase(std::remove(cont.begin(), cont.end(), toDrop ), cont.end());
-//    }
-//    std::remove_if(cont.begin(), cont.begin()+cont.size(), [toDrop, &dropCount](const T& val){if(val == toDrop){++dropCount;}; return val == toDrop;});
-//    std::cout << cont.size() << "\t" << dropCount << std::endl;
-//    for(int i = 0; i < dropCount; ++i)
-//        cont.pop_back();
+
 }
