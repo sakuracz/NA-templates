8bf0a93a5669e00a7537e62c2ff074d89ba30884
Michal Kozub <memozidozu.rinkimirikata@gmail.com>
2016-11-10 11:51:40 +0100

    Because fuck you, that's why. Changes to 22-invalid.cpp and implementation.h that added template specialization for boost::optional<T>

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <initializer_list>
+#include <boost/optional.hpp>
 #include <utility>
 
 template<typename T>
@@ -23,6 +24,30 @@ public:
 
 private:
     T _value;
+
+};
+
+template<typename T>
+class container_wrapper<boost::optional<T>>
+{
+public:
+//    container_wrapper(std::initializer_list<T> l) : elem_count(l.count())
+//    {}
+
+    container_wrapper(boost::optional<T> arg) : _value(std::move(arg)){}
+
+    container_wrapper() = default;
+    container_wrapper(const container_wrapper &) = default;
+    container_wrapper(container_wrapper&&_) = default;
+
+    container_wrapper& operator=(const container_wrapper &) = default;
+    container_wrapper& operator=(container_wrapper &&) = default;
+
+
+    size_t size() const { return _value ? 1 : 0; }
+
+private:
+    boost::optional<T> _value;
 
 };
 
